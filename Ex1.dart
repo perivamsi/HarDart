992abe8c0dc50f668fb78ed86313cc6c896a93e8
perivamsi <vamsi@vamsiperi.com>
2012-07-17 23:22:36 -0400

    Complete Ex1 and Ex2

--- Ex1.dart
+++ Ex1.dart
@@ -1,4 +1,9 @@
 void main() {
   print("Hello, World!");
   print("Hello Again");
+  print("I like typing this.");
+  print("This is fun.");
+  print("Yay! Printing.");
+  print("I'd much rather you 'not'.");
+  print('I "said" do not touch this.');
 }
---
+++ Ex2.dart
@@ -0,0 +1,14 @@
+void main(){
+  // A comment, this is so you can read your program late.r
+  // Anything after the // is ignored by Dart
+  
+  print("I could have code like this."); // and the comment after is ignored
+  
+  /*
+   * You can also use comment to "disable" or comment out a piece of code:
+   * print("This won't run");
+   * 
+   */  
+  
+  print("This will run.");
+}
